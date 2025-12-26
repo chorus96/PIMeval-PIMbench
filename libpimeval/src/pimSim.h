c38744848036466b9493ba67fb076c18af5ea6bf
chorus96 <40328697+chorus96@users.noreply.github.com>
2025-12-26 23:38:11 +0900

    Remove copyright and license from pimSim.h
    Removed copyright notice and license information from the header.

--- libpimeval/src/pimSim.h
+++ libpimeval/src/pimSim.h
@@ -1,8 +1,5 @@
 // File: pimSim.h
 // PIMeval Simulator - PIM Simulator Main Entry
-// Copyright (c) 2024 University of Virginia
-// This file is licensed under the MIT License.
-// See the LICENSE file in the root of this repository for more details.
 
 #ifndef LAVA_PIM_SIM_H
 #define LAVA_PIM_SIM_H
@@ -175,4 +172,3 @@ private:
 };
 
 #endif
-
