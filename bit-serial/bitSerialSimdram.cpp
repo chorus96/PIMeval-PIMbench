48e16ead8a8aeb70c70a50d5321c620b23e00ecb
chorus96 <40328697+chorus96@users.noreply.github.com>
2025-12-26 22:11:49 +0900

    Delete bit-serial directory

--- bit-serial/bitSerialSimdram.cpp
+++
@@ -1,7 +0,0 @@
-// Bit-Serial Performance Modeling - SIMDRAM
-// Copyright (c) 2024 University of Virginia
-// This file is licensed under the MIT License.
-// See the LICENSE file in the root of this repository for more details.
-
-#include "bitSerialSimdram.h"
-
