34522eadb706a2b83ecefa6429f8a962878b7828
chorus96 <40328697+chorus96@users.noreply.github.com>
2025-12-26 23:39:28 +0900

    Remove copyright and license from pimParamsDram.h
    Removed copyright notice and license information.

--- libpimeval/src/pimParamsDram.h
+++ libpimeval/src/pimParamsDram.h
@@ -1,8 +1,5 @@
 // File: pimParamsDram.h
 // PIMeval Simulator - DRAM parameters
-// Copyright (c) 2024 University of Virginia
-// This file is licensed under the MIT License.
-// See the LICENSE file in the root of this repository for more details.
 
 #ifndef LAVA_PIM_PARAMS_DRAM_H
 #define LAVA_PIM_PARAMS_DRAM_H
