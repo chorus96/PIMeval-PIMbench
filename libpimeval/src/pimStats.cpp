0d936f5341b98c8542e83f871bf4bae2f2fe211c
chorus96 <40328697+chorus96@users.noreply.github.com>
2025-12-28 07:37:45 +0900

    Remove copyright and license comments from pimStats.cpp
    Removed copyright notice and license information.

--- libpimeval/src/pimStats.cpp
+++ libpimeval/src/pimStats.cpp
@@ -1,8 +1,5 @@
 // File: pimStats.cpp
 // PIMeval Simulator - Stats
-// Copyright (c) 2024 University of Virginia
-// This file is licensed under the MIT License.
-// See the LICENSE file in the root of this repository for more details.
 
 #include "pimStats.h"
 #include "pimSim.h"
