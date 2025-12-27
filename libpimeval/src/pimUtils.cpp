ff1e1bae8ccf8368c8c2d4f0f86d1086b81548b3
chorus96 <40328697+chorus96@users.noreply.github.com>
2025-12-28 07:31:34 +0900

    Remove copyright and license comments from pimUtils.cpp
    Removed copyright and licensing information from the file.

--- libpimeval/src/pimUtils.cpp
+++ libpimeval/src/pimUtils.cpp
@@ -1,8 +1,5 @@
 // File: pimUtils.cc
 // PIMeval Simulator - Utilities
-// Copyright (c) 2024 University of Virginia
-// This file is licensed under the MIT License.
-// See the LICENSE file in the root of this repository for more details.
 
 #include "pimUtils.h"
 #include "libpimeval.h"
