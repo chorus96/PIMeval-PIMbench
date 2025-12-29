e209e6521284a225b0a417c4f5f793c880797f86
chorus96 <40328697+chorus96@users.noreply.github.com>
2025-12-29 21:39:37 +0900

    Update pimParamsHBMDram.cpp

--- libpimeval/src/pimParamsHBMDram.cpp
+++ libpimeval/src/pimParamsHBMDram.cpp
@@ -1,8 +1,5 @@
 // File: pimParamsHBMDram.cpp
 // PIMeval Simulator - HBM DRAM parameters
-// Copyright (c) 2024 University of Virginia
-// This file is licensed under the MIT License.
-// See the LICENSE file in the root of this repository for more details.
 
 #include "pimParamsHBMDram.h"
 #include "pimUtils.h"
