02e7b67514118f706dcc28f16854a33f5a1fe38c
chorus96 <40328697+chorus96@users.noreply.github.com>
2025-12-29 19:40:22 +0900

    pimResMgr.cpp 업데이트

--- libpimeval/src/pimResMgr.cpp
+++ libpimeval/src/pimResMgr.cpp
@@ -1,8 +1,5 @@
 // File: pimResMgr.cpp
 // PIMeval Simulator - PIM Resource Manager
-// Copyright (c) 2024 University of Virginia
-// This file is licensed under the MIT License.
-// See the LICENSE file in the root of this repository for more details.
 
 #include "pimResMgr.h"       // for pimResMgr
 #include "pimDevice.h"       // for pimDevice
